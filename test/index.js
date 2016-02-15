56c8ebe84c5b33782007266182884c36c364a288
Azat Mardan <hi@azat.co>
2016-02-15 11:36:01 -0800

    add changes :sweat_drops:

--- test/index.js
+++ test/index.js
@@ -1,24 +1,25 @@
-var output = `Account before opening:  { balance: 0, name: 'Azat Mardan', checking: '1' }
-New balance after signup bonus is 250
-Account after opening:  { balance: 1250,
-  name: 'Azat Mardan',
-  checking: '1',
-  type: 'checking' }`.replace(/\n/g, '')
+var expectedOutput = { 'Account before opening':
+   { balance: 1250,
+     name: 'Azat Mardan',
+     checking: '1',
+     type: 'checking' },
+  'New balance after signup bonus is ': 250,
+  'Account after opening':
+   { balance: 1250,
+     name: 'Azat Mardan',
+     checking: '1',
+     type: 'checking' } }
 
-var expect = require('chai').expect,
-  cp = require('child_process')
 
-describe('node account', function () {
-  it('must match the desired output', function(done){
-    child = cp.exec('node account',
-    function (error, stdout, stderr) {
-      expect(stderr).to.equal('')
-      if (error !== null) {
-        console.log('exec error: ' + error)
-      }
-      stdout = stdout.replace(/\n/g, '')
-      expect(stdout).to.equal(output)
-      done()
-    })
+var expect = require('chai').expect
+var path = require('path')
+
+describe('account.js', function () {
+  it('must return the desired output', function(done){
+    var actualOutput = require(path.join(__dirname, '..', 'account'))()
+    // console.log(actualOutput)
+    // console.log(expectedOutput)
+    expect(actualOutput).to.deep.equal(expectedOutput)
+    done()
   })
 })
