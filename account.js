78b8af1b230df084e8cc1c3f31c0bd35fa04dcbb
Bradley Reiss <bradleyreiss@Bradleys-MacBook-Air.local>
2016-12-18 16:44:06 -0500

    Done.

--- account.js
+++ account.js
@@ -7,25 +7,25 @@ module.exports = function() {
   }
 
   function setAccountType () {
-    if (customerAccount.checking === 1) {
+    if (customerAccount.checking === '1') {
       customerAccount.type = 'checking'
     } else {
       customerAccount.type = 'savings'
     }
   }
 
-  var signupBonus = 250,
-    deposit = 1000
+  var signupBonus = 250;
+  var deposit = 1000;
 
-  var openAccount = function(account, deposit) {
+  var openAccount = function(account, deposit, signupBonus) {
     account.balance += signupBonus
     output['New balance after signup bonus is '] = account.balance
     account.balance += deposit
   }
 
-  output['Account before opening'] = 'customerAccount'
+  output['Account before opening'] = customerAccount
 
-  openAccount(customerAccount)
+  openAccount(customerAccount, deposit, signupBonus)
   setAccountType()
   output['Account after opening'] = customerAccount
   return output
