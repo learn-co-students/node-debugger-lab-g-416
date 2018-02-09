439dfc5cd39b5109d8b3ae0bc5e608e275ec9920
smithwebtek <brad@smithwebtek.com>
2018-02-09 11:29:47 -0500

    Done.

--- test/index.js
+++ test/index.js
@@ -1,21 +1,27 @@
-var expectedOutput = { 'Account before opening':
-   { balance: 1250,
-     name: 'Azat Mardan',
-     checking: '1',
-     type: 'checking' },
+var expectedOutput = {
+  'Account before opening':
+    {
+      balance: 1250,
+      name: 'Azat Mardan',
+      checking: '1',
+      type: 'checking'
+    },
   'New balance after signup bonus is ': 250,
   'Account after opening':
-   { balance: 1250,
-     name: 'Azat Mardan',
-     checking: '1',
-     type: 'checking' } }
+    {
+      balance: 1250,
+      name: 'Azat Mardan',
+      checking: '1',
+      type: 'checking'
+    }
+}
 
 
 var expect = require('chai').expect
 var path = require('path')
 
 describe('account.js', function () {
-  it('must return the desired output', function(done){
+  it('must return the desired output', function (done) {
     var actualOutput = require(path.join(__dirname, '..', 'account'))()
     // console.log(actualOutput)
     // console.log(expectedOutput)
