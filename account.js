ca0c5e853e7d39702f207bbe8b37d88a2588a76a
Anastasia  Barkett <anastasia.barkett@gmail.com>
2018-07-11 17:08:42 +0000

    Done.

--- account.js
+++ account.js
@@ -3,11 +3,11 @@ module.exports = function() {
   var customerAccount = {
     balance: 0,
     name: 'Azat Mardan',
-    checking: '1'
+    checking: "1"
   }
 
   function setAccountType () {
-    if (customerAccount.checking === 1) {
+    if (customerAccount.checking === "1") {
       customerAccount.type = 'checking'
     } else {
       customerAccount.type = 'savings'
@@ -18,14 +18,14 @@ module.exports = function() {
     deposit = 1000
 
   var openAccount = function(account, deposit) {
-    account.balance += signupBonus
-    output['New balance after signup bonus is '] = account.balance
-    account.balance += deposit
+    customerAccount.balance += signupBonus
+    output['New balance after signup bonus is '] = customerAccount.balance
+    customerAccount.balance += deposit
   }
 
-  output['Account before opening'] = 'customerAccount'
+  output['Account before opening'] = customerAccount
 
-  openAccount(customerAccount)
+  openAccount(customerAccount, deposit)
   setAccountType()
   output['Account after opening'] = customerAccount
   return output
