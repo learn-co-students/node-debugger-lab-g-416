0dec2aadc6a2cd3ff005171022ca4a787f0d6480
Jared Harbison <jared.harbison@gmail.com>
2019-10-09 18:38:22 -0400

    Done.

--- account.js
+++ account.js
@@ -7,23 +7,23 @@ module.exports = function() {
   }
 
   function setAccountType () {
-    if (customerAccount.checking === 1) {
+    if (customerAccount.checking === '1') {
       customerAccount.type = 'checking'
     } else {
       customerAccount.type = 'savings'
     }
   }
 
-  var signupBonus = 250,
-    deposit = 1000
+  var signupBonus = 250
+  var deposit = 1000
 
-  var openAccount = function(account, deposit) {
+  var openAccount = function(account) {
     account.balance += signupBonus
     output['New balance after signup bonus is '] = account.balance
     account.balance += deposit
   }
 
-  output['Account before opening'] = 'customerAccount'
+  output['Account before opening'] = customerAccount
 
   openAccount(customerAccount)
   setAccountType()
