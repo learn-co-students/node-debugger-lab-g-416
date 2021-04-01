f5e549d26301e859f1135abf519458f473bc0d65
Dasha Lary <dasha4988@gmail.com>
2021-04-01 15:41:12 -0400

    Done.

--- account.js
+++ account.js
@@ -7,7 +7,8 @@ module.exports = function() {
   }
 
   function setAccountType () {
-    if (customerAccount.checking === 1) {
+    if (customerAccount.checking == 1) {
+      // debugger
       customerAccount.type = 'checking'
     } else {
       customerAccount.type = 'savings'
@@ -15,7 +16,7 @@ module.exports = function() {
   }
 
   var signupBonus = 250,
-    deposit = 1000
+  deposit = 1000
 
   var openAccount = function(account, deposit) {
     account.balance += signupBonus
@@ -23,10 +24,11 @@ module.exports = function() {
     account.balance += deposit
   }
 
-  output['Account before opening'] = 'customerAccount'
+  output['Account before opening'] = customerAccount
 
-  openAccount(customerAccount)
+  openAccount(customerAccount, deposit)
   setAccountType()
   output['Account after opening'] = customerAccount
   return output
 }
+
